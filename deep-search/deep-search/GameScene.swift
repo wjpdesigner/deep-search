997dd46430d8c8165320656aadd19accda02cb96
Jack Pardungsin <jack.pardungsin@gmail.com>
2016-06-06 00:44:04 -0400

    Day 1
    Set up the Fish grid.

--- deep-search/deep-search/GameScene.swift
+++ deep-search/deep-search/GameScene.swift
@@ -7,19 +7,115 @@
 //
 
 import SpriteKit
+import CoreMotion
 
 class GameScene: SKScene {
+    
+    var contentCreated = false
+    
+    enum FishType {
+        case A
+        case B
+        case C
+        
+        static var size: CGSize {
+            return CGSize(width: 19, height: 30)
+        }
+        
+        static var name: String {
+            return "fish"
+        }
+    }
+        
+    let kFishGridSpacing = CGSize(width: 19, height: 30)
+    let kFishRowCount = 1
+    let kFishColCount = 1
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        if (!self.contentCreated) {
+            self.createContent()
+            self.contentCreated = true
+            //motionManager.startAccelerometerUpdates()
+        }
         
-        self.addChild(myLabel)
+        //physicsWorld.contactDelegate = self
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    func createContent() {
+        
+        physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        //physicsBody!.categoryBitMask = kSceneEdgeCategory
+        
+        setupFish()
+        //setupShip()
+        //setupHud()
+        
+        // black space color
+        self.backgroundColor = SKColor.blackColor()
+    }
+    
+    func makeFishOfType(fishType: FishType) -> SKNode {
+        // 1
+        var fishColor: SKColor
+        
+        switch(fishType) {
+        case .A:
+            fishColor = SKColor.redColor()
+        case .B:
+            fishColor = SKColor.greenColor()
+        case .C:
+            fishColor = SKColor.blueColor()
+        }
+        
+        // 2
+        let fish = SKSpriteNode(color: fishColor, size: FishType.size)
+        fish.name = FishType.name
+        
+        return fish
+    }
+    
+    func setupFish() {
+        
+        // 1
+        let baseOrigin = CGPoint(x: size.width / 3, y: size.height / 2)
+        
+        for row in 0..<kFishRowCount {
+            
+            // 2
+            var fishType: FishType
+            
+            if row % 3 == 0 {
+                fishType = .A
+            } else if row % 3 == 1 {
+                fishType = .B
+            } else {
+                fishType = .C
+            }
+            
+            // 3
+            let fishPositionY = CGFloat(row) * (FishType.size.height * 2) + baseOrigin.y
+            
+            var fishPosition = CGPoint(x: baseOrigin.x, y: fishPositionY)
+            
+            // 4
+            for _ in 0..<kFishColCount {
+                
+                // 5
+                let fish = makeFishOfType(fishType)
+                fish.position = fishPosition
+                
+                addChild(fish)
+                
+                fishPosition = CGPoint(
+                    x: fishPosition.x + FishType.size.width + kFishGridSpacing.width,
+                    y: fishPositionY
+                )
+            }
+        }
+    }
+    
+    /*override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
         for touch in touches {
@@ -37,7 +133,7 @@ class GameScene: SKScene {
             
             self.addChild(sprite)
         }
-    }
+    }*/
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
